fbda5fb8271324063ddb29dd5c8fc4483e512fa6
Joel Chirackal <jchirackal19@gmail.com>
2019-01-19 17:34:31 -0500

    Notifications are not working.

---
+++ app/src/main/java/com/example/alarmmanager/AlarmToastReceiver.java
@@ -0,0 +1,14 @@
+package com.example.alarmmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AlarmToastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "This is my Alarm", Toast.LENGTH_LONG).show();
+
+    }
+}
